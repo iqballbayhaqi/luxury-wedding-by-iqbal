f349b9ba1d77d9ca8e0515be7a96b85a3790a924
iqballbayhaqi <baihaqiiqbal323@gmail.com>
2025-07-15 04:07:43 +0700

    Refactor text content to use template literals for improved readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -126,47 +126,47 @@ export default function Home() {
           />
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-24 pt-12">
             <div>
-              <p className="text-center text-2xl uppercase pb-4">HOW DID YOU MEET?</p>
+              <p className="text-center text-2xl uppercase pb-4">{`HOW DID YOU MEET?`}</p>
               <p className="text-center">
-                K: We actually had no idea exactly where and when we met until
+                {`K: We actually had no idea exactly where and when we met until
                 very recently when a friend asked us this question, ha. With
                 some Facebook sleuthing, we finally learned that we met sometime
                 in 2011 at a UCLA event with some friends. We didn't start
-                dating until over a year later!
+                dating until over a year later!`}
               </p>
             </div>
             <div>
-              <p className="text-center text-2xl uppercase pb-4">What was your first date?</p>
+              <p className="text-center text-2xl uppercase pb-4">{`What was your first date?`}</p>
               <p className="text-center">
-                C: Funny enough we consider our first date as going to church
+                {`C: Funny enough we consider our first date as going to church
                 together. It was Ash Wednesday and I just decided to text her to
                 see if she wanted to go with me to the University Catholic
                 Church. She said yes and then I smoothly asked if she wanted to
                 get dinner afterwards. We got some of the worst tofu nuggets
                 I've ever tasted in my life. But at least my romantic charm and
-                amazing sense of humor made up for it. The rest is history...
+                amazing sense of humor made up for it. The rest is history...`}
               </p>
             </div>
             <div>
               <p className="text-center text-2xl uppercase pb-4">
-                What's the first movie that you watched together?
+                {`What's the first movie that you watched together?`}
               </p>
               <p className="text-center">
-                K: Our friend Erica and I were hanging out at her dorm room one
+                {`K: Our friend Erica and I were hanging out at her dorm room one
                 night when Carlo somehow invited himself to watch the Thai
                 version of Shutter with us. If you know Carlo, he hates scary
                 movies, so there was definitely an ulterior motive there lol.
                 Erica is now fittingly our Maid of Honor for unknowingly having
                 to put up with being our third wheel (the first of many) that
-                night ;)
+                night ;)`}
               </p>
             </div>
             <div>
               <p className="text-center text-2xl uppercase pb-4">
-                What's the best meal you have eaten together?
+                {`What's the best meal you have eaten together?`}
               </p>
               <p className="text-center">
-                C: I don't know if this is the "best foodwise" but definitely
+                {`C: I don't know if this is the "best foodwise" but definitely
                 one of the "best memories at a really good restaurant." We ate
                 at Next Restaurant, a Michelin restaurant in Chicago by Grant
                 Achatz. We had no idea how many courses the meal was that Karel
@@ -174,55 +174,55 @@ export default function Home() {
                 and I swear we were expecting dessert, yet course 8 was straight
                 up steaks! Karel had 1-bite and her stomach said no more... I
                 ended up eating 2 additional courses but for the both of us,
-                including the 2 steaks.. lol
+                including the 2 steaks.. lol`}
               </p>
             </div>
             <div>
               <p className="text-center text-2xl uppercase pb-4">
-                What's the most memorable trip you've taken together?
+                {`What's the most memorable trip you've taken together?`}
               </p>
               <p className="text-center">
-                C: She would probably say our Hawaii trip back in 2019 because
+                {`C: She would probably say our Hawaii trip back in 2019 because
                 that's when she forced me to go skydiving knowing I have had a
                 huge fear of jumping out of a plane. I happened to be the very
                 last person to board the tiny plane and there were no seats so
                 they had me sit on the edge of the plane door with my legs
                 hanging out as the plane took off! Then of course I was the
                 first person to jump and I was completely terrified... She loved
-                it.
-                <br /> K: I loved it.
+                it.`}
+                <br /> {`K: I loved it.`}
               </p>
             </div>
             <div>
               <p className="text-center text-2xl uppercase pb-4">
-                What's the most spontaneous thing you've done together?
+                {`What's the most spontaneous thing you've done together?`}
               </p>
               <p className="text-center">
-                K: Carlo and I spent a lot of time going back and forth between
+                {`K: Carlo and I spent a lot of time going back and forth between
                 the East Coast and LA when he was living in Boston. While being
                 long distance was tough, we made the most of it by traveling to
                 new places during that time! My favorite would be the summer we
-                went around Spain and spontaneously extended our stay at Ibiza.
+                went around Spain and spontaneously extended our stay at Ibiza.`}
               </p>
             </div>
             <div>
               <p className="text-center text-2xl uppercase pb-4">
-                What are you on the same page about?
+                {`What are you on the same page about?`}
               </p>
               <p className="text-center">
-                C: Living life to its fullest, moment by moment. As you can tell
+                {`C: Living life to its fullest, moment by moment. As you can tell
                 it took 10 years until we were engaged! But it's not because we
                 were in doubt about each other. We've always lived our life
                 together and at our pace. We've always been ready but never in a
-                rush.
+                rush.`}
               </p>
             </div>
             <div>
               <p className="text-center text-2xl uppercase pb-4">
-                Where are you going for your honeymoon?
+                {`Where are you going for your honeymoon?`}
               </p>
               <p className="text-center">
-                C: Whatever opportunity presents itself at the right moment. ;)
+                {`C: Whatever opportunity presents itself at the right moment. ;)`}
               </p>
             </div>
           </div>
